0ce7ff15738b356334bf5cfb3431309cf8d3e341
jiaerdangjia <4201iabnomis@gmail.com>
2023-11-08 16:54:22 +0800

    你的提交消息

---
+++ Box and whisker plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+data = [25,30,25,40,45,50,55,60,65,70,75]
+
+# Create a box and whisker plot with custom colors
+box_color = 'dodgerblue'
+median_color = 'red'
+whisker_color = 'green'
+flier_color = 'purple'
+
+plt.boxplot(data,boxprops={'color': box_color},medianprops={'color':median_color},whiskerprops={'color':whisker_color},flierprops={'markerfacecolor':flier_color})
+
+# Add labels and title
+plt.xlabel('Data')
+plt.ylabel('Value')
+plt.title('Colored Box and whisker plot')
+
+# Display the plot
+plt.show()
